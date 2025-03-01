622516567191c99a1f9e27a1675b10a86a1a1361
Mohamed Bouderya <181797749+CipherXCoder@users.noreply.github.com>
2025-03-01 13:54:08 +0000

    Removed unwanted file

--- test.js
+++
@@ -1,8 +0,0 @@
-import { fileURLToPath } from "node:url";
-import path from "node:path";
-
-// Define __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-console.log(__dirname);
